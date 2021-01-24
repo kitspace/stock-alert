190fc6c26eba6e56f865000ef7f7d23cd3a5623f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-01-24 17:48:03 +0000

    Simplify crawler items/data

--- crawler/part_crawler/spiders/jlc_assembly.py
+++ crawler/part_crawler/spiders/jlc_assembly.py
@@ -2,22 +2,6 @@
 import scrapy
 import xlrd
 import logging
-from part_crawler.items import Part
-
-expected_headings = [
-    "LCSC Part",       #0
-    "First Category",  #1
-    "Second Category", #2
-    "MFR.Part",        #3
-    "Package",         #4
-    "Solder Joint",    #5
-    "Manufacturer",    #6
-    "Library Type",    #7
-    "Description",     #8
-    "Datasheet",       #9
-    "Price",           #10
-    "Stock",           #11
-]
 
 
 class JlcAssemblySpider(scrapy.Spider):
@@ -30,11 +14,13 @@ class JlcAssemblySpider(scrapy.Spider):
         sheet = book.sheet_by_index(0)
         rows = sheet.get_rows()
 
-        # make sure the columns haven't changed
         headings = list(next(rows))
-        assert [h.value for h in headings] == expected_headings
 
-        row = next(rows)
-        r = [x.value for x in row]
-        yield Part(vendor="jlc_assembly", sku=r[0], description=r[8], stock=int(r[11]))
+        for row in rows:
+            data = {}
+            for h, x in zip(headings, row):
+                data[h.value.lower()] = x.value
 
+            data["sku"] = data["lcsc part"]
+
+            yield data
